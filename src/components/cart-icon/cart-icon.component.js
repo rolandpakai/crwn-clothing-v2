d9ba2737482807df68cd34e934a08087c18c358d
rolandpakai <roland.pakai@liferay.com>
2022-12-23 17:32:56 +0100

    Implement setQuantityOfCartItems functionality

--- src/components/cart-icon/cart-icon.component.js
+++ src/components/cart-icon/cart-icon.component.js
@@ -5,13 +5,14 @@ import './cart-icon.styles.scss';
 
 const CartIcon = () => { 
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+  const { quantityOfCartItems } = useContext(CartContext);
 
   const toogleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className="cart-icon-container" onClick={toogleIsCartOpen}>
       <ShoppingIcon className='shopping-icon'/>
-      <span className="item-count">0</span>
+      <span className="item-count">{quantityOfCartItems}</span>
     </div>
   )
  }
--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
 
@@ -24,17 +24,28 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemToCart: () => {},
+  quantityOfCartItems: 0
 });
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [quantityOfCartItems, setQuantityOfCartItems] = useState(0);
+
+  useEffect(() => {
+    const newQuantityOfCartItems = cartItems.reduce((total, cartItem) => {
+      return total + cartItem.quantity 
+    }, 0);
+
+    setQuantityOfCartItems(newQuantityOfCartItems);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
+    //setQuantityOfCartItems(quantityOfCartItems + 1);
     setCartItems(addCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, quantityOfCartItems};
 
   return (
     <CartContext.Provider value={ value }>
