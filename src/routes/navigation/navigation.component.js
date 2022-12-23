7ea9f297bec35597a37733ece8a65c86a853812c
rolandpakai <roland.pakai@liferay.com>
2022-12-23 16:04:41 +0100

    Implement CartDropdown, CartIcon components

--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -2,11 +2,15 @@ import { Outlet, Link } from 'react-router-dom';
 import { Fragment, useContext } from 'react';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
@@ -27,7 +31,11 @@ const Navigation = () => {
               </Link>
             )
           }
+          <CartIcon />
         </div>
+          {isCartOpen && 
+            <CartDropdown />
+          }
       </div>
       <Outlet />
     </Fragment>
