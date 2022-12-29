b0e708307cd6053f576431f5dbf41a6e31f69618
rolandpakai <roland.pakai@liferay.com>
2022-12-29 14:24:11 +0100

    Use custom logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,21 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
-const middleware = [logger];
-const composedEnhanders = compose(applyMiddleware(...middleware));
+const loggerMiddleware = (store) => (next) => (action) => {
+  if(!action.type) {
+    return next(action);
+  }
 
-export const store = createStore(rootReducer, undefined, composedEnhanders);
+  console.log('type', action.type);
+  console.log('payload', action.payload);
+  console.log('currentState', store.getState());
+
+  next(action);
+
+  console.log('next state', store.getState());
+}
+
+const middlewares = [loggerMiddleware];
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
