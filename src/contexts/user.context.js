a8c1aa6ee358105e0773ce8ba87c56d69b1b45b9
rolandpakai <roland.pakai@liferay.com>
2022-12-29 08:49:20 +0100

    Use useReducer in user.context

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { onAuthStateChangedListener } from '../utils/firebase/firebase.utils';
 
 // Actual value
@@ -7,8 +7,36 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+  const { type, payload } = action; 
+  console.log("userReducer");
+  switch(type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER: return {
+      ...state,
+      currentUser: payload
+    }
+    default: throw new Error(`Unhandled type: ${type}`)
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+};
+
 export const UserProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE)
+  const { currentUser } = state;
+  const setCurrentUser = (user) => {
+    dispatch({
+      type: USER_ACTION_TYPES.SET_CURRENT_USER,
+      payload: user,
+    });
+  }
+  
   const value = {
     currentUser,
     setCurrentUser
