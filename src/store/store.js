0769c8b9efe154dfff40e9883c6396a445832609
rolandpakai <roland.pakai@liferay.com>
2022-12-29 17:02:10 +0100

    Use Redux DevTools browser extension

--- src/store/store.js
+++ src/store/store.js
@@ -4,20 +4,7 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if(!action.type) {
-    return next(action);
-  }
-
-  console.log('type', action.type);
-  console.log('payload', action.payload);
-  console.log('currentState', store.getState());
-
-  next(action);
-
-  console.log('next state', store.getState());
-}
+import { loggerMiddleware } from './middleware/logger';
 
 const persistConfig = {
   key: 'root',
@@ -28,8 +15,16 @@ const persistConfig = {
 const persistedReducer = persistReducer(
   persistConfig, rootReducer);
 
-const middlewares = [loggerMiddleware];
-const composedEnhancers = compose(applyMiddleware(...middlewares));
+const middlewares = [
+  process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const composeEnhancer = (
+  process.env.NODE_ENV !== 'production' && 
+  window && 
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middlewares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
 export const persistor = persistStore(store);
