db50b0b2fd9698b0ca7bbdbdbef19f2bbf10a2c7
rolandpakai <roland.pakai@liferay.com>
2022-12-31 18:27:22 +0100

    Migrate index, App, reportWebVitals, setupTests to typescript

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.util';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
