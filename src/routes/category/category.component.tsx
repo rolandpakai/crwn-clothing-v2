51a40afafa268f1b36e1956993c57a1e6a51454b
rolandpakai <roland.pakai@liferay.com>
2023-01-01 11:52:38 +0100

    Migrate category.component to typescript

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -7,11 +7,15 @@ import ProductCard from "../../components/product-card/product-card.component";
 
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
+type CategoryRouteParams = {
+  category: string;
+}
+
 const Category = () => {
   const categoriesMap = useSelector(selectCategoriesMap);
   const isLoading = useSelector(selectCategoriesIsLoading);
   
-  const { category } = useParams();
+  const { category } = useParams<keyof CategoryRouteParams>() as CategoryRouteParams;
   const [ products, setProducts ] = useState(categoriesMap[category]);
 
   useEffect(() =>{
