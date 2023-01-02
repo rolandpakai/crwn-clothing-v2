9deccc1ff42937f1f3e2448259f0dfb520717832
rolandpakai <roland.pakai@liferay.com>
2023-01-02 11:25:04 +0100

    Migrate stripe.util to typescript

--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -5,7 +5,6 @@ import { Routes, Route } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import { fetchCategoriesStart } from '../../store/categories/category.action';
-import './shop.styles.scss';
 
 const Shop = () => { 
   const dispatch = useDispatch();
